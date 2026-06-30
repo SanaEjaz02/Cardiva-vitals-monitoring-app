10b26e63735b39f17c015762219c4a5a9ddea319
KhansaBatool-54 <amina5256s@gmail.com>
2026-06-30 23:36:05 +0500

    Gradle issue resolve

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,15 +33,6 @@ android {
         versionName = flutter.versionName
     }
 
-    splits {
-        abi {
-            isEnable = true
-            reset()
-            include("arm64-v8a", "armeabi-v7a", "x86_64")
-            isUniversalApk = false
-        }
-    }
-
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
